06968bf3aba8627f29b71e7844bbe172a03f0126
Ronaldo Bicca <ronaldo.bicca@gmail.com>
2024-06-24 08:08:03 -0300

    Cap 50 e muitos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,7 @@ import { TasksComponent } from './tasks/tasks.component';
 export class AppComponent {
 
     users = DUMMY_USERS;
-    selectedUserId = 'u1';
+    selectedUserId?: string;
 
     onSelectUser(id: string){
         //console.log('selecionado ' + id);
